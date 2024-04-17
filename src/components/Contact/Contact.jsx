bfccaf4e0fbb217685ff4f82d5d4d850394b1f4c
MimesisV <141736479+MimesisV@users.noreply.github.com>
2024-04-17 17:01:28 +0300

    add icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,20 @@
+import { IoPersonSharp } from 'react-icons/io5';
+import { FaSquarePhone } from 'react-icons/fa6';
+
 export default function Contact({ data }) {
   return (
     <>
-      <p>{data.name}</p>
-      <p>{data.number}</p>
-      <button type="button" name='delete'>Delete</button>
+      <p>
+        <IoPersonSharp />
+        {data.name}
+      </p>
+      <p>
+        <FaSquarePhone />
+        {data.number}
+      </p>
+      <button type="button" name="delete">
+        Delete
+      </button>
     </>
   );
 }
